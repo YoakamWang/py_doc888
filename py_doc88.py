1464a896d347d43bca6928c752dcfce9315bfa46
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2023-03-16 08:35:36 +0800

    Add files via upload

--- py_doc88.py
+++ py_doc88.py
@@ -1,89 +1,103 @@
+import sys
+
 from selenium import webdriver
 import os
 import time
 
-from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from lxml import etree
 
-chromeOptions = webdriver.ChromeOptions()
-# browser=webdriver.Chrome(executable_path=r"C:\sources\chromedriver_win32\chromedriver.exe")
-path = os.getcwd() + '\data'
-options = Options()
-# 判断文件夹是否存在，不存在创建文件夹
-is_exists = os.path.exists(path)
-if not is_exists:
-    os.mkdir(path)
 
-# 指定浏览器下载文件夹 #"download.default_directory": path 下载文件存的路径，，，
-# "profile.default_content_setting_values.automatic_downloads":1设置下载多个文件，免得弹出框在下载过程中出现    牛牛牛
-prefs = {"download.default_directory": path, "profile.default_content_setting_values.automatic_downloads": 1}
-options.add_experimental_option("prefs", prefs)
-browser = webdriver.Chrome(chrome_options=options, executable_path=r"C:\sources\chromedriver_win32\chromedriver.exe")
-# 指定网页链接
+def main(url):
+    chromeOptions = webdriver.ChromeOptions()
+    # browser=webdriver.Chrome(executable_path=r"C:\sources\chromedriver_win32\chromedriver.exe")
+    path = os.getcwd() + '\data'
+    options = Options()
+    # 判断文件夹是否存在，不存在创建文件夹
+    is_exists = os.path.exists(path)
+    if not is_exists:
+        os.mkdir(path)
 
+    # 指定浏览器下载文件夹 #"download.default_directory": path 下载文件存的路径，，，
+    # "profile.default_content_setting_values.automatic_downloads":1设置下载多个文件，免得弹出框在下载过程中出现    牛牛牛
+    prefs = {"download.default_directory": path, "profile.default_content_setting_values.automatic_downloads": 1}
+    options.add_experimental_option("prefs", prefs)
+    browser = webdriver.Chrome(chrome_options=options,
+                               executable_path=r"C:\sources\chromedriver_win32\chromedriver.exe")
+    # 指定网页链接
 
-url = 'https://www.doc88.com/p-549550988532.html'
-# browser.get('https://www.doc88.com/p-5969904068700.html')  #论文
-browser.get(url)
-# 网页源代码
+    # url = 'https://www.doc88.com/p-91699973082285.html'
+    # browser.get('https://www.doc88.com/p-5969904068700.html')  #论文
+    browser.get(url)
+    # 网页源代码
 
-text = browser.page_source
-html = etree.HTML(text)
-page_num = html.xpath("//li[@class='text']/text()")[0]
-# 获取总页码数
-page_num = int(page_num.replace('/ ', ''))
-print(f'共{page_num}页')
-print(EC.visibility_of_element_located((By.XPATH, "//div[@id='continueButton']")))
-# #等待网页加载
-time.sleep(10)
-# 等待按钮
-element = WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.XPATH, "//div[@id='continueButton']")))
-element.click()
+    text = browser.page_source
+    html = etree.HTML(text)
+    page_num = html.xpath("//li[@class='text']/text()")[0]
+    # 获取总页码数
+    page_num = int(page_num.replace('/ ', ''))
+    print(f'共{page_num}页')
+    h1_text = html.xpath("//h1/text()")[1].strip()
+    with open("./name.txt",'w') as f:
+        f.write(h1_text)
+    # print(EC.visibility_of_element_located((By.XPATH, "//div[@id='continueButton']")))
+    # #等待网页加载
+    time.sleep(10)
+    # 等待按钮
+    element = WebDriverWait(browser, 30).until(
+        EC.visibility_of_element_located((By.XPATH, "//div[@id='continueButton']")))
+    element.click()
 
-# browser.find_element_by_xpath("//div[@id='continueButton']").click()
+    # browser.find_element_by_xpath("//div[@id='continueButton']").click()
 
+    js = "return action=document.body.scrollHeight"
+    # 初始化现在滚动条所在高度为0
+    height = 0
+    # 当前窗口总高度
+    new_height = browser.execute_script(js)
+    k = 0
 
-js = "return action=document.body.scrollHeight"
-# 初始化现在滚动条所在高度为0
-height = 0
-# 当前窗口总高度
-new_height = browser.execute_script(js)
-k = 0
-
-while k <= page_num:
-    for i in range(height, new_height, 3000):
-        k += 1
-        browser.execute_script('window.scrollTo(0, {})'.format(i))
-        time.sleep(1)
-        a = f"downloadPages({k}, {k})"
-        # 中间需要手动点一下运行下载多个文件
-        browser.execute_script("""function downloadPages(from, to) {
-            for (i = from; i <= to; i++) {
-                const pageCanvas = document.getElementById('page_' + i);
-                if (pageCanvas === null) break;
-                pageNo_ = i >= 10 ? ''+i:'0'+i;
-                const pageNo = pageNo_;
-                pageCanvas.toBlob(
-                    blob => {
-                        const anchor = document.createElement('a');
-                        anchor.download = 'page_' + pageNo + '.png';
-                        anchor.href = URL.createObjectURL(blob);
-                        anchor.click();
-                        URL.revokeObjectURL(anchor.href);
-                    }
-                    //, 'image/jpeg' // (*)
-                    //, 0.9          // (*)
-                );
-            }
-        };
-        """ + a)
+    while k <= page_num:
+        for i in range(height, new_height, 3000):
+            k += 1
+            browser.execute_script('window.scrollTo(0, {})'.format(i))
+            time.sleep(1)
+            a = f"downloadPages({k}, {k})"
+            # 中间需要手动点一下运行下载多个文件
+            browser.execute_script("""function downloadPages(from, to) {
+                for (i = from; i <= to; i++) {
+                    const pageCanvas = document.getElementById('page_' + i);
+                    if (pageCanvas === null) break;
+                    pageNo_ = i >= 10 ? ''+i:'0'+i;
+                    const pageNo = pageNo_;
+                    pageCanvas.toBlob(
+                        blob => {
+                            const anchor = document.createElement('a');
+                            anchor.download = 'page_' + pageNo + '.png';
+                            anchor.href = URL.createObjectURL(blob);
+                            anchor.click();
+                            URL.revokeObjectURL(anchor.href);
+                        }
+                        //, 'image/jpeg' // (*)
+                        //, 0.9          // (*)
+                    );
+                }
+            };
+            """ + a)
         # try:
         #     if EC.alert_is_present():
         #         print("1111")
         #         browser.switch_to.alert.accept()
         # except NoAlertPresentException:
         #     continue
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please try again")
+        sys.exit()
+    doc88_url = sys.argv[1]
+    main(doc88_url)
--- py_img_pdf_doc88.py
+++ py_img_pdf_doc88.py
@@ -3,16 +3,29 @@ import os
 
 # 列出文件夹里面的所有文件名和路径
 filepath = os.getcwd() + '\data'
-files = os.listdir(filepath)
-print(files)
-# 排序，防止合并后文件页面乱序
-filedict = {int(i.split('.')[0].split('_')[1]): i for i in files}
-print(filedict)
-files = [filedict[i] for i in sorted(filedict)]
-# # 文件名+路径
-files = ['./data/' + i for i in files]
-print(files)
-# # 把所有图片拼接为pdf
-with open('./testpdf1.pdf', mode='wb') as f:
-    f.write(img2pdf.convert(files))
+def img_pdf():
 
+    files = os.listdir(filepath)
+    # print(files)
+    # 排序，防止合并后文件页面乱序
+    with open("./name.txt", "r") as fb:
+        name = fb.readline()
+        print(name)
+    filedict = {int(i.split('.')[0].split('_')[1]): i for i in files}
+    # print(filedict)
+    files = [filedict[i] for i in sorted(filedict)]
+    # # 文件名+路径
+    files = ['./data/' + i for i in files]
+    # print(files)
+    #  把所有图片拼接为pdf
+    with open('./data/' + name + ".pdf", mode='wb') as f:
+        f.write(img2pdf.convert(files))
+def dele_imgs():
+    for root, dirs, files in os.walk(filepath):
+        for name in files:
+            if name.endswith(".png"):  # 填写规则
+                os.remove(os.path.join(root, name))
+            # print("Delete File: " + os.path.join(root, name))
+if __name__=="__main__":
+    img_pdf()
+    dele_imgs()
